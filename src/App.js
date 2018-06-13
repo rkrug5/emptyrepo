2a4c165c468e2a5e754b1822a99d8688bd71865a
rkrug5 <phishermankrug@hotmail.com>
2018-06-13 12:02:35 -0700

    added pics and nav

--- src/App.js
+++ src/App.js
@@ -4,17 +4,117 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light  navbar-fixed-top " style={{ background: 'red' }}>
+          <a className="navbar-brand" href="#"></a>
+
+          <h1 style={{ color: 'white' }}>Clicky</h1>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <h3> Click the pictures only once.</h3>
+              </li>
+
+            </ul>
+            <span className="navbar-text" style={{ color: 'white' }}>
+              Current Score
+        </span>
+            <span className="navbar-text" style={{ color: 'white' }}>
+              High Score
+        </span>
+          </div>
+        </nav>
         <div className="jumbotron">
-          <h1>Your Project</h1>
+          <h1>Hi. Welcome to the game.</h1>
           <p>
-            Enim adipisicing enim reprehenderit ex ullamco consectetur Lorem laboris magna exercitation.
-            Aliquip reprehenderit magna elit cillum adipisicing dolore aliquip velit. Ipsum ullamco
-            nostrud tempor eu deserunt ipsum incididunt fugiat esse ipsum. Exercitation nostrud
-            exercitation sit ex nostrud aliqua officia magna nostrud deserunt et esse eu deserunt. Non
-            dolor consequat qui ea culpa tempor magna nulla consectetur est occaecat.
+            This game will test your memory.  The objective is to click on each picture just once.  When you get to 8, you win.  However, if you click on the same image twice the game will end.
           </p>
         </div>
-        <div className="card">
+
+        <div className="container">
+
+          <div className="row">
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon1.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon2.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top border-dark" src="./images/mon3.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top border-dark" src="./images/mon4.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon5.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon6.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon7.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon8.jpg" alt="Card image cap" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        {/* <div className="card">
           <div className="img-container">
             <img alt='trump' src='./images/stand.jpg' />
           </div>
@@ -31,7 +131,7 @@ class App extends Component {
               </li>
             </ul>
           </div>
-        </div>
+        </div> */}
 
 
 
