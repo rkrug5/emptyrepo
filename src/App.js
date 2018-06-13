4017e7b7abc293bd8b69a3b141eec33040fbdaea
rkrug5 <phishermankrug@hotmail.com>
2018-06-12 17:33:56 -0700

    deployed with pics and multiple components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="jumbotron">
+          <h1>Your Project</h1>
+          <p>
+            Enim adipisicing enim reprehenderit ex ullamco consectetur Lorem laboris magna exercitation.
+            Aliquip reprehenderit magna elit cillum adipisicing dolore aliquip velit. Ipsum ullamco
+            nostrud tempor eu deserunt ipsum incididunt fugiat esse ipsum. Exercitation nostrud
+            exercitation sit ex nostrud aliqua officia magna nostrud deserunt et esse eu deserunt. Non
+            dolor consequat qui ea culpa tempor magna nulla consectetur est occaecat.
+          </p>
+        </div>
+        <div className="card">
+          <div className="img-container">
+            <img alt='trump' src='./images/stand.jpg' />
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Name:</strong>
+              </li>
+              <li>
+                <strong>Occupation:</strong>
+              </li>
+              <li>
+                <strong>Location:</strong>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+
+
+
+
+
+
       </div>
     );
   }
