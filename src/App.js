e7bdf672d715690085ef12700904664dc3f4685d
rkrug5 <phishermankrug@hotmail.com>
2018-06-14 09:38:08 -0700

    buttons and changing state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+
+  // let clickedPics = [];
+
+  // Setting the initial state of the score component
+  state = {
+    score: 0,
+    highScore: 0
+  };
+
+
+  // handleIncrement increases this.state.score by 1
+  handleScoreIncrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ score: this.state.score + 1 });
+  };
+
+
+  //THIS COPY PASTED CODE FOR TESTING/DEVLOPMENT
+  // handleDecrement decreases this.state.count by 1
+  // handleDecrement = () => {
+  // We always use the setState method to update a component's state
+  // this.setState({ count: this.state.count - 1 });
+  // };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   render() {
     return (
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light  navbar-fixed-top " style={{ background: 'red' }}>
-          <a className="navbar-brand" href="#"></a>
+          {/* <a className="navbar-brand" href="#"></a> */}
 
           <h1 style={{ color: 'white' }}>Clicky</h1>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
+
+          {//more code for testing, trying to get the score working}
+          }
+          <button className="btn btn-primary" type='primary' onClick={this.handleScoreIncrement}>Score?</button>
+
           <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav mr-auto">
+            {/* <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
                 <h3> Click the pictures only once.</h3>
               </li>
 
-            </ul>
+            </ul> */}
             <span className="navbar-text" style={{ color: 'white' }}>
+              <p className="card-text">Click Count: {this.state.score}</p>
               Current Score
         </span>
             <span className="navbar-text" style={{ color: 'white' }}>
@@ -36,59 +81,66 @@ class App extends Component {
         <div className="container">
 
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon1.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon1.jpg" alt="Monsoon 1" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon2.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon2.jpg" alt="Monsoon 2" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top border-dark" src="./images/mon3.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top border-dark" src="./images/mon3.jpg" alt="Monsoon 3" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top border-dark" src="./images/mon4.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top border-dark" src="./images/mon4.jpg" alt="Monsoon 4" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon5.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon5.jpg" alt="Monsoon 5" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon6.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon6.jpg" alt="Monsoon 6" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon7.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon7.jpg" alt="Monsoon 7" />
                 </a>
               </div>
             </div>
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="card mb-3 box-shadow border-dark">
                 <a className="charId">
-                  <img className="img-fluid card-img-top" src="./images/mon8.jpg" alt="Card image cap" />
+                  <img className="img-fluid card-img-top" src="./images/mon8.jpg" alt="Monsoon 8" />
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-3 box-shadow border-dark">
+                <a className="charId">
+                  <img className="img-fluid card-img-top" src="./images/mon9.jpg" alt="Monsoon 9" />
                 </a>
               </div>
             </div>
