4eefd49b19a5956e1378bedead383d820eb9bc0f
rkrug5 <phishermankrug@hotmail.com>
2018-07-19 18:37:38 -0700

    added firefly 500px images

--- src/cards.js
+++ src/cards.js
@@ -3,46 +3,64 @@ export default [
 	{
 		"id": 1,
 		"name": "mon1",
+		// "src": "./images/entp.jpg",
 		"src": "./images/mon1.jpg"
+
 	},
 	{
 		"id": 2,
 		"name": "mon2",
+		// "src": "./images/enfp.jpg",
 		"src": "./images/mon2.jpg"
+
 	},
 	{
 		"id": 3,
 		"name": "mon3",
+		// "src": "./images/esfj.jpg",
 		"src": "./images/mon3.jpg"
+
 	},
 	{
 		"id": 4,
 		"name": "mon4",
+		// "src": "./images/estj.jpg",
 		"src": "./images/mon4.jpg"
+
 	},
 	{
 		"id": 5,
 		"name": "mon5",
+		// "src": "./images/esfp.jpg",
 		"src": "./images/mon5.jpg"
+
 	},
 	{
 		"id": 6,
 		"name": "mon6",
+		// "src": "./images/isfp.jpg",
 		"src": "./images/mon6.jpg"
+
 	},
 	{
 		"id": 7,
 		"name": "mon7",
+		// "src": "./images/entj.jpg",
 		"src": "./images/mon7.jpg"
+
 	},
 	{
 		"id": 8,
 		"name": "mon8",
+		// "src": "./images/enfj.jpg"
 		"src": "./images/mon8.jpg"
+
 	},
 	{
 		"id": 9,
 		"name": "mon9",
+		// "src": "./images/intj.jpg",
 		"src": "./images/mon9.jpg"
+
 	}
 ]
